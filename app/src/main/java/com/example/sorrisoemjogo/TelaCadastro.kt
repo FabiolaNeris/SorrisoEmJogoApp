5467cfebf15a7d3cf6287bf0daadf860eb8893ad
Fabiola Neris <fabiolafalcao.n@gmail.com>
2024-11-05 21:59:38 -0300

    tela cadastro, criar user

--- app/src/main/java/com/example/sorrisoemjogo/TelaCadastro.kt
+++ app/src/main/java/com/example/sorrisoemjogo/TelaCadastro.kt
@@ -1,20 +1,57 @@
 package com.example.sorrisoemjogo
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.sorrisoemjogo.databinding.ActivityMainBinding
+import com.example.sorrisoemjogo.databinding.ActivityTelaCadastroBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class TelaCadastro : AppCompatActivity() {
+    private val binding by lazy{
+        ActivityTelaCadastroBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tela_cadastro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+
+        intent.getStringExtra("email")?.let{
+            binding.digiteEmail.setText(it)
+        }
+        intent.getStringExtra("senha")?.let {
+            binding.digiteSenha.setText(it)
+        }
+
+        binding.btnCadastrar.setOnClickListener{
+            val email = binding.digiteEmail.text.toString()
+            val senha = binding.digiteSenha.text.toString()
+
+            if(email.isEmpty() || senha.isEmpty()){
+                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            auth.createUserWithEmailAndPassword(email,senha)
+                .addOnCompleteListener(this){task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(this, "Cadastro realizado com sucesso",
+                            Toast.LENGTH_SHORT).show()
+                        finish()
+                    }else{
+                        Toast.makeText(this, "Erro: ${task.exception?.message}",
+                        Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
     }
 }
